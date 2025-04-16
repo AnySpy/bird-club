1baf4a0b477cf94d16947537da6fd88b10ac4f3b
samuelyoung2200 <spy8s@missouristate.edu>
2025-04-16 13:23:06 -0500

    Github pages setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://thebirdclubmsu.com'
+    site: 'https://anyspy.github.io',
+    base: 'bird-club',
 });
