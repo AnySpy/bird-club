a289a60a090942275cfe5f9c8283d12b51ed0b0a
samuelyoung2200 <spy8s@missouristate.edu>
2025-04-16 13:41:00 -0500

    Try 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://thebirdclubmsu.com',
+    site: 'https://www.thebirdclubmsu.com',
 });
