cb80b9a1077e43eea34c952bbefea2fc30d3f922
samuelyoung2200 <spy8s@missouristate.edu>
2025-04-16 13:37:02 -0500

    Attempting to publish to Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://anyspy.github.io',
-    base: 'bird-club',
+    site: 'https://thebirdclubmsu.com',
 });
